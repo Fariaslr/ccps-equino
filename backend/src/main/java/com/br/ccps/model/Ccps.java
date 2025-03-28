a110c61fdee2ca062712088c03f3d875d967efac
Fariaslr <lucasfarias485@gmail.com>
2025-03-28 09:11:26 -0300

    Adicionando os diagramas :hammer:

--- backend/src/main/java/com/br/ccps/model/Ccps.java
+++ backend/src/main/java/com/br/ccps/model/Ccps.java
@@ -43,4 +43,109 @@ public class Ccps {
 
 	@Column(name = "data_validade")
 	private Date dataValidade;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNomeCCPS() {
+		return nomeCCPS;
+	}
+
+	public void setNomeCCPS(String nomeCCPS) {
+		this.nomeCCPS = nomeCCPS;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getNomeVeterinario() {
+		return nomeVeterinario;
+	}
+
+	public void setNomeVeterinario(String nomeVeterinario) {
+		this.nomeVeterinario = nomeVeterinario;
+	}
+
+	public String getCrmv() {
+		return crmv;
+	}
+
+	public void setCrmv(String crmv) {
+		this.crmv = crmv;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCodigoAprovado() {
+		return codigoAprovado;
+	}
+
+	public void setCodigoAprovado(String codigoAprovado) {
+		this.codigoAprovado = codigoAprovado;
+	}
+
+	public Date getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(Date dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+	
 }
